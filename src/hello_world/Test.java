b3a9f7ffef67d8b213741df4adf9093be343cc66
chenlt-qtl <chenlt@chinanetcenter.com>
2016-05-30 10:58:45 +0800

    测试冲突

--- src/hello_world/Test.java
+++ src/hello_world/Test.java
@@ -3,7 +3,6 @@ package hello_world;
 public class Test {
 
 	public static void main(String[] args){
-		System.out.println("hello,I'm from master!");
-		System.out.println("hello,I'am from b1!");
+		int master;
 	}
 }
